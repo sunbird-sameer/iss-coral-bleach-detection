d0772c68781b74670c79e4519c81b40764951a4d
sunbird-sameer <sunbird.sameer@gmail.com>
2022-07-10 13:28:35 +0530

    update

---
+++ iss_PhotoCapture_pyppeteer.py
@@ -0,0 +1,19 @@
+import asyncio
+from pyppeteer import launch
+from time import sleep
+import iss_location
+
+async def asyncmain():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('https://iss-live.github.io')
+    sleep(20)
+    await page.screenshot({'path': str("'" + iss_location.lat() + '_' + iss_location.long() + '.png' + "'"), 'fullPage': 'true'})
+    await browser.close()
+
+def main():
+    asyncio.get_event_loop().run_until_complete(asyncmain())
+
+
+if __name__ == "main":
+    main()
